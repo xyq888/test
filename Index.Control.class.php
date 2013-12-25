4d6a3e4b8a6327103112601bab52dda8d46a6c61
houdunwang-hd <232119349@qq.com>
2013-12-25 16:17:02 +0800

    瞎创建的

---
+++ Index.Control.class.php
@@ -0,0 +1,9 @@
+<?php
+//测试控制类
+class Indexontrol extends Control{
+	function index(){
+		echo 33434;
+	}
+	
+}
+?>
---
+++ admin.php
@@ -0,0 +1,5 @@
+<?php
+define("DEBUG",true);
+define("APP_PATH","admin/");
+define("DIR_SAFE",ture);
+require "./hdphp/hdphp/hdphp.php";
